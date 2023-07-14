fd3daf280cfe52f7552863cbe2b0b665b4c66075
Frederick Manlusoc <frederickmanlusoc@merchantsfleet.com>
2023-07-14 16:17:06 +0800

    Assignment Session 3

---
+++ Session3/EFCoreAssignment.Data/AppDbContextInitialiser.cs
@@ -0,0 +1,101 @@
+﻿using EFCoreAssignment.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFCoreAssignment.Data
+{
+    public class AppDbContextInitialiser
+    {
+        private readonly ILogger<AppDbContextInitialiser> logger;
+        private readonly AppDbContext context;
+
+
+        public AppDbContextInitialiser(ILogger<AppDbContextInitialiser> logger, AppDbContext context)
+        {
+            this.logger = logger;
+            this.context = context;
+        }
+
+        public async Task InitialiseAsync()
+        {
+            try
+            {
+                if (this.context.Database.IsSqlServer())
+                {
+                    await this.context.Database.MigrateAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+#pragma warning disable CA1848 // Use the LoggerMessage delegates
+                this.logger.LogError(ex, "An error occurred while initializing the database.");
+#pragma warning restore CA1848 // Use the LoggerMessage delegates
+                throw;
+            }
+        }
+
+        public async Task SeedAsync()
+        {
+            try
+            {
+                await this.TrySeedAsync();
+            }
+            catch (Exception ex)
+            {
+#pragma warning disable CA1848 // Use the LoggerMessage delegates
+                this.logger.LogError(ex, "An error occurred while seeding the database.");
+#pragma warning restore CA1848 // Use the LoggerMessage delegates
+                throw;
+            }
+        }
+
+        public async Task TrySeedAsync()
+        {
+            // Default data
+            // Seed, if necessary for shops table
+            if (!this.context.Shops.Any())
+            {
+                this.context.Shops.Add(new Shop()
+                {
+                    Name = "SM Online",
+                });
+
+                this.context.Shops.Add(new Shop()
+                {
+                    Name = "Zalora",
+                });
+
+                this.context.Shops.Add(new Shop()
+                {
+                    Name = "My Shop",
+                });
+
+                await this.context.SaveChangesAsync();
+            }
+
+            // Seed, if necessary for products table
+            if (!this.context.Products.Any())
+            {
+                this.context.Products.Add(new Product()
+                {
+                    Name = "TShirt",
+                    ShopId = 3
+                });
+
+                this.context.Products.Add(new Product()
+                {
+                    Name = "Pants",
+                    ShopId = 2
+                });
+
+                this.context.Products.Add(new Product()
+                {
+                    Name = "Sun Glasses",
+                    ShopId = 1
+                });
+
+                await this.context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Session3/EFCoreAssignment.Services/ProductService.cs
@@ -0,0 +1,77 @@
+﻿using EFCoreAssignment.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreAssignment.Data.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ProductService(AppDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<ProductDto>> GetProducts()
+        {
+            // TODO get products
+            return await _dbContext.Products.Select(x => new ProductDto(x.Id, x.Name, x.ShopId)).ToListAsync();
+        }
+
+        public async Task<ProductDto> GetProduct(int id)
+        {
+            // TODO get product
+            var product = await _dbContext.Products.FirstOrDefaultAsync(x => x.Id == id) 
+                    ?? throw new NullReferenceException($"Product with id = {id} not found.");
+
+            return new ProductDto(product.Id, product.Name, product.ShopId);
+        }
+
+        public async Task<int> CreateProduct(CreateProductDto productForCreation)
+        {
+            // TODO create a product
+            var product = new Product
+            {
+                Name = productForCreation.Name,
+                ShopId = productForCreation.ShopId
+            };
+
+            _dbContext.Add(product);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateProduct(UpdateProductDto productForUpdate)
+        {
+            //TODO update a product
+            var product = await _dbContext.Products.SingleOrDefaultAsync(x => x.Id == productForUpdate.Id) 
+                ?? throw new NullReferenceException($"Product with id = {productForUpdate.Id} not found.");
+
+            product.Name = productForUpdate.Name;
+            product.ShopId = productForUpdate.ShopId;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            //TODO delete a product
+            var product = await _dbContext.Products.SingleOrDefaultAsync(x => x.Id == id) 
+                ?? throw new NullReferenceException($"Product with id = {id} not found.");
+
+            _dbContext.Remove(product);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetProducts();
+        Task<ProductDto> GetProduct(int id);
+        Task<int> CreateProduct(CreateProductDto productForCreation);
+        Task UpdateProduct(UpdateProductDto productForUpdate);
+        Task DeleteProduct(int id);
+    }
+
+    public record ProductDto(int Id, string Name, int ShopId);
+    public record CreateProductDto(string Name, int ShopId);
+    public record UpdateProductDto(int Id, string Name, int ShopId);
+}
